be0085195f27090f4d87f365ba6c0b0a9a1198d9
abdulqadirtr <qadirabdul20@yahoo.com>
2024-06-24 02:45:05 +0200

    * card and button clicks are added.

--- app/src/main/java/app/compose/recyclerviewcompose/MainActivity.kt
+++ app/src/main/java/app/compose/recyclerviewcompose/MainActivity.kt
@@ -1,93 +1,180 @@
 package app.compose.recyclerviewcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.compose.recyclerviewcompose.ui.theme.RecyclerViewComposeTheme
+
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: ObjectViewModel by viewModels()
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RecyclerViewComposeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                  CourseListScreen(viewModel = viewModel)
-                }
+
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun ListItem(name : String){
+fun MyApp(modifier: Modifier = Modifier) {
+    /**
+     * It behaves similarly to remember, but the stored value will survive the activity or process
+     * recreation using the saved instance
+     * state mechanism (for example it happens when the screen is rotated in the Android application).
+     */
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
-    var showMore by remember { mutableStateOf(true) }
-    var extraPadding = if (showMore) 48.dp else 0.dp
+    Surface(modifier, color = MaterialTheme.colors.background) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
 
-
-   Surface(color = MaterialTheme.colors.primary,
-    modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp )) {
-
-        Column(modifier = Modifier
-            .padding(24.dp)
-            .fillMaxWidth()
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!", fontSize = 24.sp)
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
         ) {
+            Text("Continue")
+        }
+    }
+}
 
-            Row {
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
 
-                Column(modifier = Modifier
-                    .weight(1f)
-                    .padding(start = 1.dp)) {
+@Composable
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+    }
+}
 
-                    Text(text = "Course")
-                    Text(text = name)
-                }
-                OutlinedButton(onClick = { showMore = !showMore }) {
-                    Text(if (showMore) "Show less" else "Show more" )
-                }
+@Composable
+private fun CardContent(name: String) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name, style = MaterialTheme.typography.h3.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+            if (expanded) {
+                Text(
+                    text = ("Composem ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4),
+                )
             }
-
         }
-
-    }
-}
-
-@Composable
-fun CourseListScreen(viewModel: ObjectViewModel) {
-    val objects by viewModel.objects.collectAsState()
-
-    LazyColumn (modifier = Modifier.padding(vertical = 4.dp)){
-        items(objects) { course ->
-            ListItem(name = course.name)
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.Search else Icons.Filled.Add,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+            )
         }
     }
 }
 
-
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
-fun DefaultPreview() {
+fun GreetingPreview() {
     RecyclerViewComposeTheme {
-
+        Greetings()
     }
 }
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    RecyclerViewComposeTheme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    RecyclerViewComposeTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+
+
+
+
